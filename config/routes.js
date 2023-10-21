4b4c1eaf81e40c8067f04ffc1ab0dba1d080f637
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-10-21 14:14:18 +0300

    Home page

---
+++ config/routes.js
@@ -0,0 +1,17 @@
+const cookieParser = require("cookie-parser");
+const homeController = require("../controllers/homeController.js");
+// const userController = require("../controllers/userController.js");
+// const { auth } = require("../middlewares/authMiddleware.js");
+// const creatureController = require("../controllers/creatureController.js");
+module.exports = (app) => {
+  app.use(cookieParser())
+//   app.use(auth)
+  app.use(homeController);
+//   app.use("/user", userController);
+//   app.use("/creature", creatureController);
+//   app.use('*',(req,res)=>{
+//     res.render('404')
+//   })
+
+
+};
---
+++ controllers/homeController.js
@@ -0,0 +1,22 @@
+// const { getAll } = require("../services/animalService.js");
+
+const { errorHelper } = require("../utils/errorHelpers.js");
+
+const homeController = require("express").Router();
+
+homeController.get("/", async (req, res) => {
+  try {
+    res.render("home", {
+      title: "Home",
+      
+    });
+  } catch (err) {
+    const errors = errorHelper(err)
+    res.render('home',{
+      title : 'Home',
+      errors
+    })
+  }
+});
+
+module.exports = homeController;
