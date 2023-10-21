ca00951cb3602f953d1335eb63e684756f5e2c14
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-10-21 13:31:45 +0300

    Add utils pre exam

---
+++ utils/tokenHelper.js
@@ -0,0 +1,18 @@
+const jwt = require('./jwt.js')
+const SECRET_KEY = require('../config/secretKey.js')
+
+//DON'T FORGET TO ALTER THE PAYLOAD DEPENDING ON THE TASK
+
+async function createToken(user){
+    const payload = {
+        _id: user._id,
+        username : user.username,
+        email: user.email,
+    }
+
+    const token = await jwt.sign(payload,SECRET_KEY)           
+
+    return token
+}
+
+module.exports = createToken
